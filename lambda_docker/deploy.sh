0122fec83730702a806ecb83f840011077b5d065
denys <denysburimov@gmai.com>
2023-08-02 21:15:10 +0300

    working commit

---
+++ lambda_docker/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ../.env
+# echo ${AWS_ACCESS_KEY}
+# Run the get-login-password command to authenticate the Docker CLI to your Amazon ECR registry.
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 071964951237.dkr.ecr.us-east-1.amazonaws.com
+
+# Create a repository in Amazon ECR using the create-repository command.
+aws ecr create-repository --repository-name hello-world --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+
+# Extract repositoryUri using jq and assign to a variable
+repositoryUri=$(echo "$output" | jq -r '.repository.repositoryUri')
+
+# Print the repositoryUri
+echo "Repository URI: $repositoryUri"
