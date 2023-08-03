86174abc8e01634220b552361973b96f188924b9
denys <denysburimov@gmai.com>
2023-08-03 11:26:03 +0300

    ecr creds

--- lambda_docker/deploy_image.sh
+++ lambda_docker/deploy_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t burn_model --platform linux/amd64 . &&
-docker tag burn_model:latest 867661782732.dkr.ecr.us-east-1.amazonaws.com/burn_model:latest &&
-docker push 867661782732.dkr.ecr.us-east-1.amazonaws.com/burn_model:latest &&
-aws lambda update-function-code --function-name oculo_burn_model --image-uri 867661782732.dkr.ecr.us-east-1.amazonaws.com/burn_model:latest
+docker build -t lambda_01 --platform linux/amd64 . &&
+docker tag lambda_01:latest 071964951237.dkr.ecr.us-east-1.amazonaws.com/s2b-lambda_01:latest &&
+docker push 071964951237.dkr.ecr.us-east-1.amazonaws.com/s2b-lambda_01:latest &&
+aws lambda update-function-code --function-name oculo_burn_model --image-uri 071964951237.dkr.ecr.us-east-1.amazonaws.com/s2b-lambda_01:latest
